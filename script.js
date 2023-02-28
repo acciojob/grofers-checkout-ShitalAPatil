85fef8e73b6274709d12d595287a6e1c03c9ff2d
ShitalAPatil <shitalpatilb29@gmail.com>
2023-02-28 14:50:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-const getSumBtn = document.createElement("button");
-getSumBtn.append("Get Total Price");
-document.body.appendChild(getSumBtn);
+// const getSumBtn = document.createElement("button");
+// getSumBtn.append("Get Total Price");
+// document.body.appendChild(getSumBtn);
 
-const getSum = () => {
-//Add your code here
+// const getSum = () => {
+// //Add your code here
   
-};
+// };
 
-getSumBtn.addEventListener("click", getSum);
+// getSumBtn.addEventListener("click", getSum);
 
+const calculateTotalBtn = document.getElementById('calculate-total-btn');
+		calculateTotalBtn.addEventListener('click', calculateTotal);
+
+		function calculateTotal() {
+			const prices = document.querySelectorAll('.price');
+			let totalPrice = 0;
+			prices.forEach(price => {
+				totalPrice += parseFloat(price.textContent);
+			});
+			const totalPriceCell = document.getElementById('ans');
+			totalPriceCell.textContent = totalPrice.toFixed(2);
+		}
